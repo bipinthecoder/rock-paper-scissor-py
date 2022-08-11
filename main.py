a68c2847d3a95961f9e54afafcae8dc53fe40f61
BIPIN BABU <bipinbabu@d060222.stir.ac.uk>
2022-08-11 19:27:00 +0100

    Added comments and indentation

--- main.py
+++ main.py
@@ -3,12 +3,15 @@
 import random
 
 
+# Function to initiate the game
 def game_initialize():
     game_rounds = int(input('Enter the number of rounds\n'))
 
     computer_score = 0
     person_score = 0
     game_aborted = False
+
+    # Run the game till all the rounds are exhausted
     for game_round in range(game_rounds):
         game_result = run_game()
         if game_result == -1:
@@ -18,6 +21,7 @@ def game_initialize():
             computer_score += game_result[0]
             person_score += game_result[1]
 
+    # Conditions to show the result
     if game_aborted:
         print("Game Aborted!")
         return 0
@@ -30,12 +34,14 @@ def game_initialize():
     print(f'Scoring: \n' + f'You: {person_score}\n' + f'Computer: {computer_score}\n')
 
 
+# Function to run the game - core logic
 def run_game():
-    game_dict = {1: 'Rock', 2: 'Paper', 3: 'Scissor'}
+    game_dict = {1: 'Rock', 2: 'Paper', 3: 'Scissor'}  # The choices
     invalid_moves = 0
     player_choice = ''
     comp_score = 0
     person_score = 0
+    # Giving 5 chances if invalid inputs are given
     for i in range(5):
         player_choice = input('Enter your move\n')
         if player_choice not in game_dict.values():
@@ -46,10 +52,12 @@ def run_game():
         else:
             break
 
+    # Logic for computer's move
     computer_choice = random.randint(1, 3)
     computer_choice = game_dict.get(computer_choice)
     print(f'computer played : {computer_choice}')
 
+    # Logic for deciding the score
     if computer_choice == 'Rock' and player_choice == 'Paper':
         person_score += 1
     elif computer_choice == 'Rock' and player_choice == 'Scissor':
@@ -65,7 +73,9 @@ def run_game():
     else:
         pass
 
+    # Returning the score tuple
     return comp_score, person_score
 
 
+# Start the game
 game_initialize()
